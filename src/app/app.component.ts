7597e2173e90b9c234342f1deeda9b819db133ac
ramchendar28 <112571700+ramchendar28@users.noreply.github.com>
2023-02-15 22:46:11 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,46 @@
 import { Component } from '@angular/core';
-import { Store } from '@ngrx/store'
-import * as globalState from './global.state'
-import * as appState from './ngrx-store/app.state'
-import * as appActions from './ngrx-store/app.actions'
+import { Store } from '@ngrx/store';
+import * as globalState from './global.state';
+import * as appState from './ngrx-store/app.state';
+import * as appActions from './ngrx-store/app.actions';
+import { HttpClient } from '@angular/common/http';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   isLoggedIn: boolean;
   isLoading: boolean;
   username: string;
+  userList: any = [];
 
-  constructor(private _store: Store<globalState.IGlobalState>) { }
+  constructor(
+    private _store: Store<globalState.IGlobalState>,
+    private appService: AppService
+  ) {}
   ngOnInit() {
-    this._store.select(appState.usernameSelector).subscribe(payload => {
+    this._store.select(appState.usernameSelector).subscribe((payload) => {
       if (payload.length > 0) {
         this.username = payload;
         this.isLoggedIn = true;
         this._store.dispatch(new appActions.IsLoadingAction(false));
       }
-    })
-    this._store.select(appState.isLoadingStateSelector).subscribe(payload => {
+    });
+    this._store.select(appState.isLoadingStateSelector).subscribe((payload) => {
       this.isLoading = payload;
-    })
+    });
   }
 
   Login() {
-    this._store.dispatch(new appActions.loginAction())
+    this._store.dispatch(new appActions.loginAction());
     this._store.dispatch(new appActions.IsLoadingAction(true));
+  }
+  getusers() {
+    this.appService.getdata().subscribe((res: any) => {
+      this.userList = res;
+    });
   }
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,14 +1,18 @@
-import { Injectable } from '@angular/core'
-import { of } from 'rxjs'
-import { delay } from 'rxjs/operators'
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AppService {
+  constructor(private http: HttpClient) {}
   Login() {
-    return of('Saksham').pipe(
-      delay(1000)
-    )
+    return of('Saksham').pipe(delay(1000));
+  }
+
+  getdata(): Observable<any> {
+    return this.http.get('https://jsonplaceholder.typicode.com/users');
   }
 }
